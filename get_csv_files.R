12607464f365d041ef05194438237d88afb6dbcf
Unknown <jamieayton@live.co.uk>
2018-07-30 16:12:41 +0100

    re-write get script
    improve readability, use better packages and methods

--- get_csv_files.R
+++ get_csv_files.R
@@ -3,170 +3,233 @@
 
 # 1. Setup ----------------------------------------------------------------
 
-# global options
-options(stringsAsFactors = FALSE)
-
 # packages
 library('tidyverse')
-library('stringr')
+library('lubridate')
+library('furrr')
 library('rvest')
+library('here')
 
 # dir & create /data/ subdir if doesn't exist
-project_wd <- getwd()
-data_dir <- paste0(project_wd, "/raw_data")
+project_dir <- here::here()
+data_dir <- file.path(project_dir, "raw_data")
 if(dir.exists(data_dir)==FALSE){dir.create(data_dir)}
 
 
-# 2. Create list of csv files ---------------------------------------------
+
+# 2. Competitions List ----------------------------------------------------
 
 # page urls for different countries
-page_urls <- tibble(
-  country = c("england", "scotland", "germany", "italy", "spain", "france", "netherlands", "belgium", "portugal", "turkey", "greece")
+page_urls <- 
+  tribble(
+    ~country, 
+    "england", 
+    "scotland", 
+    "germany", 
+    "italy", 
+    "spain", 
+    "france", 
+    "netherlands", 
+    "belgium", 
+    "portugal", 
+    "turkey", 
+    "greece"
   ) %>% 
   mutate(
     url = paste0("http://www.football-data.co.uk/", country, "m.php")
   )
 
 
+
+# 3. Scrape Competitions --------------------------------------------------
+
 # tbl of csv urls - to be populated via loop
-csv_urls <- tibble(
-  country = character(), last_update = character(), url = character()
-)
+csv_urls <- 
+  tibble(
+    country = character(), 
+    last_update = character(), 
+    url = character()
+  )
 
 # loop through page_urls and populate csv_urls
-for (i in seq(1L, nrow(page_urls))){
+for (i in seq_along(page_urls$url)){
   
   # read page and wait to make sure not hitting server too regularly
   webpage <- read_html(page_urls$url[i])
-  Sys.sleep(2.5 + runif(1))
+  Sys.sleep(2.5 + 0.5*runif(1))
   
   # parse data from the webpage
-  csv_urls_i <- tibble(
-    country = webpage %>% html_nodes("p:nth-child(1) > b") %>% html_text() %>% gsub("Data Files: ", "", .), 
-    last_update = webpage %>% html_nodes("p:nth-child(1) > i") %>% html_text() %>% gsub("Last updated: \t", "", .) %>% as.character(.),
-    url = webpage %>% html_nodes(css="a") %>% html_attr('href') %>% subset(., grepl(pattern=".csv", .)) %>% paste0("http://www.football-data.co.uk/", .)
-  )
-    
+  csv_urls_i <- 
+    tibble(
+      country = webpage %>% 
+        html_nodes(css = "p:nth-child(1) > b") %>% 
+        html_text() %>% 
+        str_replace_all("Data Files: ", ""), 
+      last_update = webpage %>% 
+        html_nodes("p:nth-child(1) > i") %>% 
+        html_text() %>% 
+        str_replace_all("Last updated: \t", "") %>% 
+        as.character(),
+      url = webpage %>% 
+        html_nodes(css = "a") %>% 
+        html_attr('href') %>% 
+        str_subset(pattern = ".csv$") %>% 
+        paste0("http://www.football-data.co.uk/", .)
+    )
+  
   # add rows for i to existing csv_urls tibble
   csv_urls <- csv_urls %>% 
     bind_rows(., csv_urls_i)
-
-
+  
+  # rm
   rm(csv_urls_i)
   rm(webpage)
   
 }
 
+# check completion
+stopifnot(
+  identical(i, length(page_urls$url))
+)
 rm(i)
 
 
 # parse url to get season_code & league_code
 csv_urls <- csv_urls %>% 
   mutate(
-    season_code = str_split(url, "/", simplify=TRUE)[,5], 
-    season_code = paste0(
-      str_sub(season_code, 1, str_length(season_code)/2), "-", str_sub(season_code, 1+str_length(season_code)/2, str_length(season_code))
-      ), 
-    league_code = str_split(url, "/", simplify=TRUE)[,6], 
-    league_code = str_replace_all(league_code, "\\.csv", "")
+    parsed_url = url, 
+    parsed_url = parsed_url %>% str_replace_all("http://www.football-data.co.uk/", ""), 
+    parsed_url = parsed_url %>% str_replace_all(".csv", "")
+  ) %>% 
+  separate(
+    parsed_url, into = c("string", "season_code", "league_code"), sep = "\\/"
+  ) %>% 
+  select(
+    country, last_update, url, league_code, season_code
   )
 
 # create a filename for the csv file
 csv_urls <- csv_urls %>% 
   mutate(
     file_code = paste0(league_code, "_", season_code), 
-    file_name = paste0(data_dir, "/", file_code, ".csv")
+    file_name = file.path(data_dir, paste0(file_code, ".csv")) %>% as.character()
   )
 
 
 
+# 4. Download Files -------------------------------------------------------
 
-
-# 3. Get csv files --------------------------------------------------------
-
-get_csv_files <- function(url, file_name){
+# loop through and download csv files
+for (i in seq_along(csv_urls$url)){
   
-  # read the csv from url
-  csv_temp <- read.csv(url)
+  # download via read_csv
+  csv_temp <- read.csv(csv_urls$url[i])
   
   # convert to tibble
   csv_temp <- as.tibble(csv_temp)
   
-  # wait to prevent hitting server too regularly
-  Sys.sleep(2.5 + runif(1))
-  
   # write csv
-  write_csv(csv_temp, file_name)
+  write_csv(csv_temp, csv_urls$file_name[i])
   
+  # rm
   rm(csv_temp)
+  
+  # wait to prevent hitting server too regularly
+  Sys.sleep(2.5 + 1.5*runif(1))
+  
+  # progress
+  print(paste0(i, " of ", nrow(csv_urls)))
 }
 
-# download csv files via loop
-for (i in seq(1L, nrow(csv_urls))){
-  
-  # get csv file
-  get_csv_files(csv_urls$url[i], csv_urls$file_name[i])
-  print(i)
-}
+# check completion
+stopifnot(
+  identical(i, nrow(csv_urls))
+)
 rm(i)
 
 
-# write csv_urls
-write_csv(csv_urls, file.path(paste0("csv_urls", ".csv")))
 
-
-
-# 4. Create parsed df with universal format -------------------------------
+# 5. Create parsed df with universal format -------------------------------
 
 # read data
-football_data <- map(csv_urls$file_name, read_csv) %>% 
-  setNames(., csv_urls$file_code)
-
-
-# desired columns to keep
-desired_columns <- c(
-  "Div", "Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR", "HS", "AS", "HST", "AST", 
-  "B365H", "B365D", "B365A", "PSH", "PSD", "PSA", "PSCH", "PSCD", "PSCA", 
-  "BbMxH", "BbMxD", "BbMxA", "BbAvH", "BbAvD", "BbAvA", "BbMx.2.5", "BbMx.2.5.1", "BbAv.2.5", "BbAv.2.5.1", 
-  "BbAHh", "BbMxAHH", "BbMxAHA", "BbAvAHH" , "BbAvAHA"
-)
-
-# drop NAs
-# select desired columns
-# create id link back to original csv
-# force formatting before binding
-# drop NAs for critical cols
-football_data <- map(
-    seq_along(football_data), 
-    function(x) football_data[[x]] %>% 
-      select(one_of(desired_columns[desired_columns %in% colnames(.)])) %>% 
-      mutate(
-        id = names(football_data)[x]
-      ) %>% 
-      mutate(
-        BbAHh = as.character(if(exists('BbAHh', where=.)){BbAHh} else {NA}), 
-        PSCH = as.numeric(if(exists('PSCH', where=.)){PSCH} else {NA})
-      )
+football_data <- csv_urls %>% 
+  select(league_code, season_code, file_name) %>% 
+  mutate(
+    data = future_map(file_name, function(x) read_csv(x, col_types = cols(.default = col_character())))
   ) %>% 
-  bind_rows(.) %>% 
-  bind_cols(., str_split(.$id, "_", simplify=TRUE) %>% as.tibble()) %>% 
-  rename(league_code = V1, season_code = V2) %>% 
-  drop_na(one_of("Div", "Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR"))
+  select(-file_name) %>% 
+  unnest()
 
-rm(desired_columns)
+# clean data & select columns
+football_data <- football_data %>% 
+  drop_na("Div") %>% 
+  mutate(
+    HomeTeam = if_else(is.na(HomeTeam) & !is.na(HT), HT, HomeTeam), 
+    AwayTeam = if_else(is.na(AwayTeam) & !is.na(AT), AT, AwayTeam)
+  ) %>% 
+  select(
+    league_code, season_code, 
+    Div, Date, 
+    ends_with("Team"), 
+    starts_with("FT"), 
+    ends_with("S"), 
+    ends_with("ST"), 
+    starts_with("B365"), 
+    starts_with("BbAv"), 
+    starts_with("BbMx"), 
+    starts_with("BbAH")
+  ) %>% 
+  select(
+    -starts_with("B365."), 
+    -starts_with("B365A")
+  )
+
+# change col types
+football_data <- football_data %>% 
+  mutate(
+    Date = dmy(Date)
+  ) %>% 
+  mutate_at(
+    .vars = c("FTHG", "FTAG", "HS", "AS", "HST", "AST"), 
+    .funs = function(x) x %>% as.integer()
+  ) %>% 
+  mutate_at(
+    .vars = vars(starts_with("B365")), 
+    .funs = function(x) x %>% as.numeric()
+  ) %>% 
+  mutate_at(
+    .vars = vars(starts_with("Bb"), -starts_with("BbAH")), 
+    .funs = function(x) x %>% as.numeric()
+  )
+
+# fix incorrect data
+football_data <- football_data %>% 
+  mutate_at(
+    .vars = vars(starts_with("B365|BbAv|BbMx")), 
+    .funs = function(x) if_else(near(x, 0), NA, x)
+  ) %>% 
+  mutate_at(
+    .vars = vars(starts_with("B365|BbAv|BbMx")), 
+    .funs = function(x) if_else(x < 1.0, NA, x)
+  )
 
 
-# write
-write_csv(football_data, paste0("football_data", ".csv"))
+
+# 6. Write Data -----------------------------------------------------------
+
+# write csv files
+write_csv(page_urls, file.path(paste0("page_urls", ".csv")))
+write_csv(csv_urls, file.path(paste0("csv_urls", ".csv")))
+write_csv(football_data, file.path(paste0("football_data", ".csv")))
 
 
 
+# 7. Cleanup --------------------------------------------------------------
 
-# 5. Cleanup --------------------------------------------------------------
-
+# data
 rm(football_data, csv_urls, page_urls)
-rm(project_wd, data_dir)
-rm(get_csv_files)
 
+# dir
+rm(project_dir, data_dir)
 
 
